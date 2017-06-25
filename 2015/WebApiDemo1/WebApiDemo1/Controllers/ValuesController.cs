bdce42a497b13e771dc5470f27ebe36c829584ae
bsspriya <bsspriya1@gmail.com>
2017-06-25 13:48:16 +0800

    VisualStudioProjectSamples

---
+++ 2015/WebApiDemo1/WebApiDemo1/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiDemo1.Controllers
+{
+    //[Authorize]
+    public class ValuesController : ApiController
+    {
+        static List<string> lstStrings = new List<string>() {
+        "Value0","Value1","Value2"};
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return lstStrings;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return lstStrings[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            lstStrings.Add(value);
+
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            lstStrings[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            lstStrings.RemoveAt(id);
+        }
+    }
+}
---
+++ 2017/Bot3/Bot3/Controllers/BotFrameworkFaqDialog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QnAMakerDialog;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Threading.Tasks;
+
+namespace Bot3.Controllers
+{
+    [Serializable]
+    [QnAMakerService("5fcfe2c94c7c4cafb99a78479baa5a36", "5e6464e4-09c7-4cd9-9f47-b5afee440073")]
+    public class BotFrameworkFaqDialog : QnAMakerDialog<object>
+    {
+        public override async Task NoMatchHandler(IDialogContext context, string originalQueryText)
+        {
+            await context.PostAsync($"Sorry, I couldn't find an answer for '{originalQueryText}'.");
+            context.Wait(MessageReceived);
+        }
+
+        [QnAMakerResponseHandler(50)]
+        public async Task LowScoreHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+        {
+            await context.PostAsync($"I found an answer that might help...{result.Answer}.");
+            context.Wait(MessageReceived);
+        }
+
+    }
+}
